d6071a149c061ec94db4544d577aac3c6ca22a34
tarumt-Jiaye <145625246+tarumt-Jiaye@users.noreply.github.com>
2024-10-03 23:19:47 +0800

    dwa

---
+++ 2D TD Assignment/Assets/SoundManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource sfxSource;
+    public AudioClip death;
+    public AudioClip sound;
+
+    private void Start()
+    {
+        
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
